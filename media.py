49952c9ce0717c45e512db480e3ffc44e2a522ff
unknown <ryanebebel@gmail.com>
2024-08-09 14:54:36 -0300

    Adicionado uma nova condicional

--- media.py
+++ media.py
@@ -7,4 +7,11 @@ nota2 = float(input('Digite a nota 2 ...'))
 
 media = (nota1 + nota2) / 2
 
+if media > 5 :
+    print('Aprovado!')
+elif media > 3 and media < 5 :
+    print('Recuperação')
+else :
+    print('Reprovado!')
+
 print('A média é...', media)
