2178c03c66d3bdcb2c97291d9cf530145482851a
unknown <ryanebebel@gmail.com>
2024-08-09 14:36:28 -0300

    first commit

---
+++ media.py
@@ -0,0 +1,10 @@
+nota1 : float
+nota2 : float
+media : float
+
+nota1 = float(input('Digite a nota 1 ...'))
+nota2 = float(input('Digite a nota 2 ...'))
+
+media = (nota1 + nota2) / 2
+
+print('A média é...', media)
